28688b65f1960ce685c6cdd69477192905646992
zeze1004 <thwjd2717@naver.com>
2020-08-28 00:07:31 +0900

    이미지 풀스크린

--- views/Loading.js
+++ views/Loading.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
-import { StyleSheet, ImageBackground } from 'react-native'
+import { StyleSheet, Image, ImageBackground } from 'react-native'
 
 import styles from '../styles/style';
 
+
+let image = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+  }
+});
+
+
+
 export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Loading...</Text>
+    <View style={[styles.row, styles.header]}>
+      <Image source={require('../assets/splash.png')} 
+      style= {image.backgroundImage}
+      />
+      {/* <ImageBackground source={{uri: imageURL}} style={styles.bgImage} /> */}
       <StatusBar style="auto" />
     </View>
   );
