a724d2dc00397f8411b6d76b987e3f91cb6a188e
zeze1004 <thwjd2717@naver.com>
2020-08-27 23:33:52 +0900

    배경사진 올리기

--- views/Loading.js
+++ views/Loading.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native'
 
 import styles from '../styles/style';
 
