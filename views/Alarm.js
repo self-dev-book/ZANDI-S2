6f81347ae36fe25c5dfb2a0f09e736d3869bdb59
KoDaHyeon <wus2363@gmail.com>
2020-09-02 00:25:26 +0900

    control alarm at a time

--- views/Alarm.js
+++ views/Alarm.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, Image, Text, View } from 'react-native';
-import ReactNativeSettingsPage, { 
-	SectionRow, 
+import ReactNativeSettingsPage, {
+    SectionRow,
     NavigateRow,
     SwitchRow,
-	CheckRow
+    CheckRow
 } from 'react-native-settings-page';
 import RNPickerSelect, { defaultStyles } from 'react-native-picker-select';
 
@@ -46,66 +46,74 @@ const period = [
 class Alarm extends React.Component {
     state = {
         check: false,
-        switch_alarm: false,
+        switch_alarm: true,
         switch_sound: false,
         switch_vibe: false,
         switch_push: false,
-        value: 40,
         alarm_period: undefined
     }
 
     inputRefs = {
         alarm_period: null
     }
-    
 
-	render() {
-		return (
-			<ReactNativeSettingsPage>
-                <SectionRow>
-                    <SwitchRow
-                        text='알람'
-                        _value={this.state.switch_alarm}
-                        _onValueChange={() => {
-                            this.setState({
-                                switch_alarm: !this.state.switch_alarm,
-                                switch_push: false
-                            })
-                        }} />
-                    <SwitchRow
-                        text='소리'
-                        _value={this.state.switch_sound}
-                        _onValueChange={() => { this.setState({ switch_sound: !this.state.switch_sound }) }} />
-                    <SwitchRow
-                        text='진동'
-                        _value={this.state.switch_vibe}
-                        _onValueChange={() => { this.setState({ switch_vibe: !this.state.switch_vibe }) }} />
-                    <SwitchRow 
-                        text='푸시알림' 
-                        _value={this.state.switch_push}
-                        _onValueChange={() => { this.setState({ switch_push: !this.state.switch_push }) }} />
-                </SectionRow>
 
-                <SectionRow
-                    text="알람 주기" >
-                    <View paddingVertical={5} />
-                    <RNPickerSelect  
-                        items={period}
-                        placeholder={{
-                            label: '알람 주기를 선택하세요'
-                        }}
-                        onValueChange={value => {
-                            this.setState({
-                                alarm_period: value
-                            });
-                        }}
-                        style={pickerSelectStyles}
-                        value={this.state.alarm_period}
-                        ref={el => {
-                            this.inputRefs.alarm_period = el;
-                        }}
-                    />
-                </SectionRow>
+    render() {
+        return (
+            <ReactNativeSettingsPage>
+                <SwitchRow
+                    text='알람'
+                    _value={this.state.switch_alarm}
+                    _onValueChange={() => {
+                        this.setState({
+                            switch_alarm: !this.state.switch_alarm,
+                        })
+                    }} />
+                {this.state.switch_alarm ?
+                    (
+                        <>
+                      
+                            <SectionRow>
+                                <SwitchRow
+                                    text='소리'
+                                    _value={this.state.switch_sound}
+                                    _onValueChange={() => { this.setState({ switch_sound: !this.state.switch_sound }) }} />
+                                <SwitchRow
+                                    text='진동'
+                                    _value={this.state.switch_vibe}
+                                    _onValueChange={() => { this.setState({ switch_vibe: !this.state.switch_vibe }) }} />
+                                <SwitchRow
+                                    text='푸시알림'
+                                    _value={this.state.switch_push}
+                                    _onValueChange={() => { this.setState({ switch_push: !this.state.switch_push }) }} />
+                            </SectionRow>
+                            <SectionRow
+                                text="알람 주기" >
+                                <View paddingVertical={5} />
+                                <RNPickerSelect
+                                    items={period}
+                                    placeholder={{
+                                        label: '알람 주기를 선택하세요'
+                                    }}
+                                    onValueChange={value => {
+                                        this.setState({
+                                            alarm_period: value
+                                        });
+                                    }}
+                                    style={pickerSelectStyles}
+                                    value={this.state.alarm_period}
+                                    ref={el => {
+                                        this.inputRefs.alarm_period = el;
+                                    }}
+                                />
+                            </SectionRow>
+                        </>
+                    ) :
+                    (
+                        <>
+                        </>
+                    )}
+
             </ReactNativeSettingsPage>
         )
     }
@@ -114,25 +122,25 @@ class Alarm extends React.Component {
 
 const pickerSelectStyles = StyleSheet.create({
     inputIOS: {
-      fontSize: 16,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 4,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
     },
     inputAndroid: {
-      fontSize: 16,
-      paddingHorizontal: 10,
-      paddingVertical: 8,
-      borderWidth: 0.5,
-      borderColor: 'purple',
-      borderRadius: 8,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
+        fontSize: 16,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        borderWidth: 0.5,
+        borderColor: 'purple',
+        borderRadius: 8,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
     },
-  });
+});
 
 export default Alarm
