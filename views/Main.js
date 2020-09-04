6a4f124770c8e6ab7523c0085c9e128f6c0264b0
Geonho Kim <gunhoflash@gmail.com>
2020-09-04 23:31:07 +0900

    Fix error and beautify

--- views/Main.js
+++ views/Main.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Platform, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { Container, Content, Header, Left, Body, Right, Button } from 'native-base';
 import ReactNativeSettingsPage, {
 	SectionRow,
 	NavigateRow,
 	CheckRow
 } from 'react-native-settings-page';
-
-import { NavigationActions } from 'react-navigation';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -17,30 +14,34 @@ var images = [
 ]
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={{flex:2, justifyContent: 'flex-end', flexDirection: 'row'}}>
-            <Icon name="ios-notifications" size={30} color="black" />
-						<Icon name="ios-settings" size={30} color="black" style={{paddingRight:20, paddingLeft:20}} onPress={() => navigation.navigate('Setting')} />
-          </View>
-          <StatusBar style="auto" />
-        </View>
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusBar style="auto" />
+				<View style={styles.header}>
+					<View style={{flex:2, justifyContent: 'flex-end', flexDirection: 'row'}}>
+						<Icon name="ios-notifications" size={30} color="black" />
+						<Icon name="ios-settings" size={30} color="black" style={{paddingRight:20, paddingLeft:20}} onPress={() => this.props.navigation.navigate('Setting')} />
+					</View>
+				</View>
 				<View style={styles.commit}>
 					<View style={{flexDirection:'row', justifyContent:'space-around'}}>
+
 						<View style={{alignItems:'center'}}>
 							<Text style={{fontSize:17, fontWeight:'bold'}}>5</Text>
 							<Text style={{fontSize:12, color:'gray', textAlign: 'center'}}>week</Text>
 						</View>
+
 						<View style={{alignItems:'center'}}>
 							<Text style={{fontSize:17, fontWeight:'bold'}}>18</Text>
 							<Text style={{fontSize:12, color:'gray'}}>month</Text>
 						</View>
-							<View style={{alignItems:'center'}}>
+
+						<View style={{alignItems:'center'}}>
 							<Text style={{fontSize:17, fontWeight:'bold'}}>192</Text>
 							<Text style={{fontSize:12, color:'gray'}}>year</Text>
 						</View>
+
 					</View>
 				</View>
 				<View style={styles.announcement} >
@@ -50,32 +51,32 @@ export default class App extends React.Component {
 				</View>
 				<View style={styles.character}>
 					<Image
-						source={require('../assets/zandi_nobackground.png')} style={{width:150, hegiht:50, resizeMode:'contain'}}
+						source={require('../assets/zandi_nobackground.png')} style={{width:150, height:150, resizeMode:'contain'}}
 					/>
 				</View>
 				<View style={styles.bottom}>
-				<Text>(슬라이드 화면전환)</Text>
+					<Text>(슬라이드 화면전환)</Text>
 				</View>
-      </View>
-    );
-  }
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+container: {
+	flex: 1,
 		alignItems: 'center',
-    backgroundColor: '#fff',
-  },
+		backgroundColor: '#fff',
+	},
 	header: {
 		flex:1,
-    justifyContent: 'flex-start',
+		justifyContent: 'flex-start',
 		backgroundColor: '#acd99d',
-    height: 55,
-    flexDirection: 'row',
-    paddingTop: 15,
-		paddingBottom: 10
-  },
+		height: 55,
+		flexDirection: 'row',
+		paddingTop: 15,
+			paddingBottom: 10
+	},
 	commit: {
 		flex:2,
 		justifyContent: 'center',
